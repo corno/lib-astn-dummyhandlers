90ed291c4536cca0c5ced1548d12500dc0dda1ff
corno <corno.astn.a@schraverus.com>
2023-02-19 15:46:51 +0100

    .

--- pareto/src/data/project.data.ts
+++ pareto/src/data/project.data.ts
@@ -2,11 +2,11 @@ import * as pd from 'pareto-core-data'
 
 import * as mproject from "lib-pareto-typescript-project/dist/submodules/project"
 
-const d = pd.wrapRawDictionary
+const d = pd.d
 
 import { $ as api } from "./api.data"
 
-export const $: mproject.T.Project = {
+export const $: mproject.T.Project<pd.SourceLocation> = {
     'author': "Corno",
     'description': "create a handler that does nothing more than consume the message",
     'license': "ISC",

c62cab1969daa564d387553aba5a4fc6900596cc
corno <corno.astn.a@schraverus.com>
2023-03-20 09:14:52 +0100

    .

--- typescript/pub/src/main/implementation.generated.ts
+++ typescript/pub/src/main/implementation.generated.ts
@@ -1,5 +1,5 @@
 import { API } from "./api.generated"
-import { $$ as icreateDummyRequiredValueHandler } from "./implementations/createDummyRequiredValueHandler.p"
+import { $$ as icreateDummyRequiredValueHandler } from "./implementations/createDummyRequiredValueHandler.a.c"
 
 export const $api: API = {
     'createDummyRequiredValueHandler': icreateDummyRequiredValueHandler,

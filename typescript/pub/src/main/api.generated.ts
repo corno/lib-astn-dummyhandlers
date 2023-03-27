0e6bf4da5518327c087fb7144cd3ac734ffda8f9
corno <corno.astn.a@schraverus.com>
2023-03-27 13:44:13 +0200

    .

--- typescript/pub/src/main/api.generated.ts
+++ typescript/pub/src/main/api.generated.ts
@@ -2,6 +2,10 @@ import * as pt from 'pareto-core-types'
 
 import * as g_this from "./glossary"
 
+export namespace D {
+    
+}
+
 export namespace A {
     
     export type createDummyRequiredValueHandler = <GAnnotation>() => g_this.ASYNC.A.C.CreateDummyRequiredValueHandler<GAnnotation>

6523d04ed833facbe4dd814fbaf43cd100b2baa7
corno <corno.astn.a@schraverus.com>
2023-04-11 15:37:42 +0200

    .

--- typescript/pub/src/main/implementations/createDummyRequiredValueHandler.a.c.ts
+++ typescript/pub/src/main/implementations/createDummyRequiredValueHandler.a.c.ts
@@ -7,60 +7,44 @@ export const $$: A.createDummyRequiredValueHandler = () => {
     return {
         'construct': () => {
             function createDummyValueHandler<TokenAnnotation>(): g_h.ASYNC.I.ValueHandler<TokenAnnotation> {
-    
+
                 function createDummyObjectHandler(): g_h.ASYNC.I.ObjectHandler<TokenAnnotation> {
-                    return {
-                        data: {
-                            property: () => {
-                                return createDummyRequiredValueHandler()
-                            },
-                            anonymousProperty: () => {
-                                return createDummyValueHandler()
-                            },
+                    return ({
+                        'data': {
+                            'property': () => createDummyRequiredValueHandler(),
+                            'anonymousProperty': () => createDummyValueHandler(),
                         },
-                        end: () => { },
-                    }
+                        'end': () => { },
+                    })
                 }
                 function createDummyArrayHandler(): g_h.ASYNC.I.ArrayHandler<TokenAnnotation> {
                     return {
-                        data: () => {
-                            return createDummyValueHandler()
-                        },
-                        end: () => { }
+                        'data': () => createDummyValueHandler(),
+                        'end': () => { }
                     }
                 }
                 function createDummyTaggedUnionHandler(): g_h.ASYNC.I.TaggedUnionHandler<TokenAnnotation> {
                     return {
-                        option: () => createDummyRequiredValueHandler(),
-                        missingOption: () => createDummyRequiredValueHandler(),
+                        'option': () => createDummyRequiredValueHandler(),
+                        'missingOption': () => createDummyRequiredValueHandler(),
                     }
                 }
                 return {
-                    object: () => {
-                        return createDummyObjectHandler()
-                    },
-                    array: () => {
-                        return createDummyArrayHandler()
-                    },
-                    taggedUnion: () => {
-                        return createDummyTaggedUnionHandler()
-                    },
-                    simpleString: () => {
-                    },
-                    multilineString: () => {
-    
-                    }
+                    'object': () => createDummyObjectHandler(),
+                    'array': () => createDummyArrayHandler(),
+                    'taggedUnion': () => createDummyTaggedUnionHandler(),
+                    'simpleString': () => { },
+                    'multilineString': () => { }
                 }
             }
-    
+
             function createDummyRequiredValueHandler<TokenAnnotation>(): g_h.ASYNC.I.RequiredValueHandler<TokenAnnotation> {
                 return {
-                    missing: () => {
-                    },
-                    exists: createDummyValueHandler()
+                    'missing': () => { },
+                    'exists': createDummyValueHandler()
                 }
             }
-    
+
             return createDummyRequiredValueHandler()
         }
     }
